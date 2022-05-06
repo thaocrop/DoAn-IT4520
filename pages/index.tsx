a846db3f439b1e0f1f26c1e25a7dece4a547e9d9
james.nguyen <thaocrop@gmail.com>
2022-05-06 10:51:40 +0700

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,7 @@ const Home: NextPageWithLayout = () => {
                 <div className="container mx-auto items-center flex flex-wrap">
                     <div className="w-full md:w-8/12 lg:w-6/12 xl:w-6/12 px-4">
                         <div className="pt-32 sm:pt-0">
-                            <h2 className="font-semibold text-4xl text-emerald-800">
+                            <h2 className="font-semibold text-4xl text-emerald-700">
                                 Giới thiệu chung
                             </h2>
                             <h1 className="font-semibold text-6xl text-teal-500">
--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -3,42 +3,42 @@ const queryString = require("query-string");
 import { store } from "src/redux";
 
 const axiosClient = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+    baseURL: process.env.REACT_APP_API_URL,
 
-  headers: {
-    "Content-Type": "application/json",
-    "X-Requested-With": "XMLHttpRequest",
-  },
+    headers: {
+        "Content-Type": "application/json",
+        "X-Requested-With": "XMLHttpRequest",
+    },
 
-  paramsSerializer: (params) => queryString.stringify(params),
+    paramsSerializer: (params) => queryString.stringify(params),
 });
 
 axiosClient.interceptors.request.use((config) => {
-  // const token = await getFirebaseToken();
-  // if (token) {
-  //   config.headers.Authorization = `Bearer ${token}`;
-  //
-  const token = store.getState().auth.auth?.accessToken;
-  config.headers["access-token"] = token;
+    // const token = await getFirebaseToken();
+    // if (token) {
+    //   config.headers.Authorization = `Bearer ${token}`;
+    //
+    const token = store.getState().auth.auth?.accessToken;
+    config.headers["access-token"] = token;
 
-  return config;
+    return config;
 });
 
 axiosClient.interceptors.response.use(
-  (response) => {
-    if (response && response.data) {
-      return response;
-    }
-    return response;
-  },
+    (response) => {
+        if (response && response.data) {
+            return response;
+        }
+        return response;
+    },
 
-  (error: any) => {
-    // Handle errors
-    if (error.response && error.response.status === 401) {
-      //logout
+    (error: any) => {
+        // Handle errors
+        if (error.response && error.response.status === 401) {
+            //logout
+        }
+        throw error;
     }
-    throw error;
-  }
 );
 
 export default axiosClient;
